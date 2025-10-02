74ba4daea05f57745f5e29fd0543ffa3773dc182
annikakoons <annika.koons@laverne.edu>
2025-10-02 11:32:18 -0700

    CMPS Assignment 1 (10/2)
    This code will display an organized schedule of all of my classes at LaVerne. Information includes days, times, location, course information, and professor.

---
+++ Assignment 1/Assignment 1.cpp
@@ -0,0 +1,25 @@
+// Assignment 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "================================================================================================" << endl;
+	cout << "				   Fall 2025 Class Schedule" << endl;
+	cout << "					   Annika Koons" << endl;
+	cout << "================================================================================================" << endl<<endl;
+	cout << "|Day	|Time		|Course#|Course Name		|Location		|Instrcutor" << endl;
+	cout << "------------------------------------------------------------------------------------------------" << endl;
+	cout << "|MWF	|9:25-10:30	|M328	|Abstract Algebra	|Founders Hall 216	|Y. Daneshbod" << endl;
+	cout<< "|MWF	|10:35-11:40	|M315	|Differential Equations	|Founders Hall 216	|Y. Daneshbod" << endl;
+	cout<< "|MW	|1:10-2:45	|P350	|Optics			|Founders Hall 21	|V. Preisler" << endl;
+	cout << "|TTR	|10:20-12:55	|C301	|Programming Concepts	|Founders Hall 218	|J. Rodriguez" << endl;
+	cout << "|T	|3:30-5:00	|M499B	|Senior Seminar		|Miller Hall 208	|Y. Daneshbod" << endl;
+	cout << "|TRF	|1:00-5:30	|M104	|Math Emporium		|Barkley 28		|F. Ives" << endl;
+	cout << "------------------------------------------------------------------------------------------------" << endl<<endl;
+	cout << "Total Credits: 18" << endl;
+	cout << "Please bring a lunch." << endl;
+	cout << "================================================================================================" << endl << endl;
+
+}
